576858d8543f57c6591c5029801bfa0e758e1e77
Nico Garce <ngarce36@gmail.com>
2026-08-26 10:55:59 +0800

    refactor: move DB credentials to untracked db-config.php

--- includes/connection.php
+++ includes/connection.php
@@ -2,21 +2,14 @@
 
 require_once __DIR__ . '/base-url.php';
 
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASSWORD = '';
-$DATABASE_NAME = 'saliksik';
+$configFile = __DIR__ . '/db-config.php';
 
+if (!is_file($configFile)) {
+    http_response_code(500);
+    exit('Missing database configuration: create includes/db-config.php (see includes/db-config.example.php).');
+}
+
+require $configFile;
 
 $connection = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASSWORD, $DATABASE_NAME);
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-
-// $DATABASE_HOST = 'localhost';
-// $DATABASE_USER = 'u231629948_saliksik_uphsl';
-// $DATABASE_PASSWORD = 'Serkingd28();';
-// $DATABASE_NAME = 'u231629948_saliksik_uphsl';
-
-// $DATABASE_HOST = 'localhost';
-// $DATABASE_USER = 'root';
-// $DATABASE_PASSWORD = '';
-// $DATABASE_NAME = 'saliksik_uphsl';
